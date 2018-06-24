1fd0be418eb74dd0a36d2fd8421d96f784fef0da
Antice <ken.r.e.madsen@gmail.com>
2018-06-24 12:32:37 +0200

    Basic Product registration form layout and link created. admin_products_controller updated with new functionality. Code status: non functional due to error, research needed to find a fix.

--- lib/squareshop_web/controllers/admin_products_controller.ex
+++ lib/squareshop_web/controllers/admin_products_controller.ex
@@ -1,7 +1,25 @@
 defmodule SquareshopWeb.AdminProductsController do
     use SquareshopWeb, :controller
 
+		alias Squareshop.Products
+		alias Squareshop.Products.Product
+
     def index(conn, _params) do
-      render conn, "index.html"
+			products = Products.list_products()
+			render conn, "index.html", products: products
     end
-  end
+
+		def new(conn, _params) do
+			changeset = Products.create_product()
+			render(conn, "new.html", changeset: changeset)
+		end
+
+		def create(conn, %{"product" => product_params}) do
+			case Products.create_product(product_params) do
+	      {:ok, product} ->
+	        conn
+	        |> put_flash(:info, "Product created successfully.")
+	        |> redirect(to: admin_products_path(conn, :show, product))
+		end
+	end
+end
