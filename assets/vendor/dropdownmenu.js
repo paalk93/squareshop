7082b7ba53096b0c3b5348fd23598840a6ba0579
paalk93 <paal_93@hotmail.com>
2018-07-05 11:25:42 +0200

    Added arrow icon to the toggle dropdown menu function.

--- assets/vendor/dropdownmenu.js
+++ assets/vendor/dropdownmenu.js
@@ -1,3 +1,11 @@
 function toggleDropdown(){
-  document.getElementsByClassName("sq-nav-dropdown-content")[0].classList.toggle("sq-show-dropdown-content")
-}
+  var dropdownbtn = document.getElementsByClassName("sq-nav-dropdown-content")[0];
+  dropdownbtn.classList.toggle("sq-show-dropdown-content")
+  var arrow = document.getElementById("togglearrow");
+    if(arrow.innerHTML == " \u2193"){
+       arrow.innerHTML = " \u2191"
+    }
+    else {
+      arrow.innerHTML = " \u2193"
+    }
+  }
